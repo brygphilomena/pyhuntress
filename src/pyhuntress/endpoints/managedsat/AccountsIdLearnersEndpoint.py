b3eae5456b2cc175e92d76d351ae240f2ff021a0
Peter Annabel <peter.annabel@gmail.com>
2025-07-29 09:56:06 -0500

    add pagination to accounts/{id}/learners

--- src/pyhuntress/endpoints/managedsat/AccountsIdLearnersEndpoint.py
+++ src/pyhuntress/endpoints/managedsat/AccountsIdLearnersEndpoint.py
@@ -1,8 +1,10 @@
 from pyhuntress.endpoints.base.huntress_endpoint import HuntressEndpoint
 from pyhuntress.interfaces import (
     IGettable,
+    IPaginateable,
 )
 from pyhuntress.models.managedsat import SATLearners
+from pyhuntress.responses.paginated_response import PaginatedResponse
 from pyhuntress.types import (
     JSON,
     HuntressSATRequestParams,
@@ -12,10 +14,43 @@ from pyhuntress.types import (
 class AccountsIdLearnersEndpoint(
     HuntressEndpoint,
     IGettable[SATLearners, HuntressSATRequestParams],
+    IPaginateable[SATLearners, HuntressSATRequestParams],
 ):
     def __init__(self, client, parent_endpoint=None) -> None:
         HuntressEndpoint.__init__(self, client, "learners", parent_endpoint=parent_endpoint)
         IGettable.__init__(self, SATLearners)
+        IPaginateable.__init__(self, SATLearners)
+
+    def paginated(
+        self,
+        page: int,
+        limit: int,
+        params: HuntressSATRequestParams | None = None,
+    ) -> PaginatedResponse[SATLearners]:
+        """
+        Performs a GET request against the /accounts/{id}/learners endpoint and returns an initialized PaginatedResponse object.
+
+        Parameters:
+            page (int): The page number to request.
+            limit (int): The number of results to return per page.
+            params (dict[str, int | str]): The parameters to send in the request query string.
+        Returns:
+            PaginatedResponse[SATLearners]: The initialized PaginatedResponse object.
+        """
+        if params:
+            params["page[number]"] = page
+            params["page[size]"] = limit
+        else:
+            params = {"page[number]": page, "page[size]": limit}
+        return PaginatedResponse(
+            super()._make_request("GET", params=params),
+            SATLearners,
+            self,
+            "data",
+            page,
+            limit,
+            params,
+        )
 
     def get(
         self,
